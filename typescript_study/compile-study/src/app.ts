4263bf58a2468afc517f3b4c275240ba0cdb38e0
mbynae <piowm123@gmail.com>
2023-10-23 20:24:59 +0900

    타입스크립트 공부중

--- typescript_study/compile-study/src/app.ts
+++ typescript_study/compile-study/src/app.ts
@@ -1,25 +1,34 @@
 interface Greetable {
-    readonly name: string;
+    readonly name?: string;
+    outputName?: string; //옵셔널 체이닝으로 선택적 변수를 지정 가능
 
     greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-    name: string;
+    name?: string; //클래스에도 옵셔널체이닝 가능
     age = 30;
 
-    constructor(name: string) {
-        this.name = name;
+    //매개변수에 옵셔널체이닝 시 초기값이 undefined로 지정됨
+    //name:string = '' 형태로 초기값을 지정해주는거도 가능
+    constructor(name?: string) {
+        if(name){
+            this.name = name;
+        }
     }
 
     greet(phrase: string) {
-        console.log(`${phrase} ${this.name}`);
+        if(this.name){
+            console.log(`${phrase} ${this.name}`);
+        } else {
+            console.log('Hi')
+        }
     }
 }
 
 let user1: Greetable;
 
-user1 = new Person('이름');
+user1 = new Person();
 //user1.name = '바꿈'; //readonly 속성 때문에 변경 불가
 
 user1.greet('이녀석의 이름은:');

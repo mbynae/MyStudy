1bfdde929c6cf9b50c12bba970a8804a9f25ee4d
mbynae <piowm123@gmail.com>
2023-11-07 19:53:06 +0900

    타스공부 - 구별된 유니언

--- typescript_study/advanced-type-study/src/app.ts
+++ typescript_study/advanced-type-study/src/app.ts
@@ -1,39 +1,62 @@
-type Admin = {
-    name: string;
-    privileges: string[];
-};
-
-type Employee = {
-    name: string;
-    startDate: Date;
-};
-
-type ElevatedEmployee = Admin & Employee;
-
-// interface Admin {
+// type Admin = {
 //     name: string;
 //     privileges: string[];
-// }
+// };
 
-// interface Employee {
+// type Employee = {
 //     name: string;
 //     startDate: Date;
-// }
+// };
 
-// interface ElevatedEmployee extends Admin, Employee {}
+// type ElevatedEmployee = Admin & Employee;
 
-const person1: ElevatedEmployee = {
-    name: '직원 이름',
-    privileges: ['프론트엔드'],
-    startDate: new Date(),
-};
+// const person1: ElevatedEmployee = {
+//     name: '직원 이름',
+//     privileges: ['프론트엔드'],
+//     startDate: new Date(),
+// };
 
-console.log(person1);
+// console.log(person1);
 
-type Combinable = string | number;
-type Numeric = number | boolean;
+// type Combinable = string | number;
+// type Numeric = number | boolean;
 
-type Universal = Combinable & Numeric;
-const example1: Universal = 10;
-// const example2: Universal = '문자열';
-// const example3: Universal = true;
+// type Universal = Combinable & Numeric;
+// const example1: Universal = 10;
+
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: 'horse';
+    runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    //console.log(animal.flyingSpeed); //새인지 말인지 몰라 에러가 발생
+
+    // if('flyingSpeed' in animal){ //기존의 in 타입가드로 입력 가능
+    //     console.log(animal.flyingSpeed);
+    // }
+
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+        default:
+            speed = 0;
+            break;
+    }
+    console.log(`${animal.type} is speed: ${speed}`);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
+moveAnimal({ type: 'horse', runningSpeed: 20 });

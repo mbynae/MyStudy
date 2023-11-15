f5e62f4d1465cf98e0e0540d481e46c111c535ee
mbynae <piowm123@gmail.com>
2023-11-15 21:09:58 +0900

    일반 함수에서 제네릭

--- typescript_study/advanced-type-study/src/app.ts
+++ typescript_study/advanced-type-study/src/app.ts
@@ -8,8 +8,30 @@
 //제네릭을 사용해 입력값의 타입을 지정해주면 에러 발생이 안나며, 유연하게 사용이 가능하다.
 //함수 안에서 연산을 할 시 제네릭의 타입을 명시해줘야 한다.
 //보통 첫번째 인자는 T, 두번째 인자는 U를 사용함
-function merge<T extends object, U extends object>(obj1: T, obj2: U) {
-    return Object.assign(obj1, obj2);
+// function merge<T extends object, U extends object>(obj1: T, obj2: U) {
+//     return Object.assign(obj1, obj2);
+// }
+// const mergeObj = merge({ name: '이름', hobby: ['sports'] }, { age: 30 });
+// console.log(mergeObj.age); //타입스크립트가 결과값이 뭔지 이해하기 때문에 에러 발생X
+
+interface Lengthy {
+    length: number
 }
-const mergeObj = merge({ name: '이름', hobby: ['sports'] }, { age: 30 });
-console.log(mergeObj.age); //타입스크립트가 결과값이 뭔지 이해하기 때문에 에러 발생X
+
+//T에 속하는 속성이 뭔지 알려줘서 정상 작동
+//문자열이든 배열이든 length 속성을 갖을 수 있다면 유연하게 전부 허용
+function countAndPrint<T extends Lengthy>(element: T){
+    let descriptionText = "값이 하나도 없음";
+    
+    //타입스크립트가 element의 속성을 모르기 때문에 length에서 타입 에러 발생
+    if(element.length === 1){
+        descriptionText = "값이 1개 있음";
+    }
+    if(element.length > 1){
+        descriptionText = `값이 ${element.length}개 있음`;
+    }
+
+    return [element, descriptionText];
+}
+console.log(countAndPrint("아무거나텍스트"));
+console.log(countAndPrint(['이름', '나이']));

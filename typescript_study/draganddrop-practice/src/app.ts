0f5401e762e5acd6f7bfa211c19e69db9760c1b1
mbynae <piowm123@gmail.com>
2023-11-23 07:48:39 +0900

    드래그앤드롭 TS

--- typescript_study/draganddrop-practice/src/app.ts
+++ typescript_study/draganddrop-practice/src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+class ProductInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    importNode2: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importNode = document.importNode(this.templateElement.content, true);
+        this.element = importNode.firstElementChild as HTMLFormElement;
+        this.importNode2 = this.templateElement.content.cloneNode(true)! as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        console.log(this.importNode2);
+        // this.hostElement.insertAdjacentElement('afterbegin', this.element);
+        // this.hostElement.insertAdjacentElement('afterbegin', this.importNode2);
+        // this.hostElement.append(this.importNode2);
+        this.hostElement.append(this.templateElement.content);
+    }
+}
+
+const prjInput = new ProductInput();

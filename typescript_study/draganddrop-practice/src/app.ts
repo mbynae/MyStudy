2213cf5bdf40cffc3c9b68dcb7d43b67a9bd6496
mbynae <piowm123@gmail.com>
2023-11-29 17:02:09 +0900

    타입스크립트 공부

--- typescript_study/draganddrop-practice/src/app.ts
+++ typescript_study/draganddrop-practice/src/app.ts
@@ -11,6 +11,36 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     return adjDescriptor;
 }
 
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    minNumber?: number;
+    maxNumber?: number;
+}
+
+function validate(validData: Validatable) {
+    let isValid = true;
+    if (validData.required) {
+        isValid = isValid && validData.value.toString().trim().length !== 0;
+    }
+    if (validData.minLength != null && typeof validData.value === 'string') {
+        isValid = isValid && validData.value.length >= validData.minLength;
+    }
+    if (validData.maxLength != null && typeof validData.value === 'string') {
+        isValid = isValid && validData.value.length <= validData.maxLength;
+    }
+    if (validData.minNumber != null && typeof validData.value === 'number') {
+        isValid = isValid && validData.value >= validData.minNumber;
+    }
+    if (validData.maxNumber != null && typeof validData.value === 'number') {
+        isValid = isValid && validData.value <= validData.maxNumber;
+    }
+
+    return isValid;
+}
+
 class ProductInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -40,18 +70,41 @@ class ProductInput {
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
-        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
-            alert("전부 입력해주세요.");
+        // if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+        //     alert("전부 입력해주세요.");
+        //     return;
+        // } else {
+        //     return [enteredTitle, enteredDescription, +enteredPeople];
+        // }
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true,
+        };
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5,
+        };
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            minNumber: 1,
+            maxNumber: 5,
+        };
+
+        if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+            alert('정확히 입력해주세요.');
             return;
         } else {
             return [enteredTitle, enteredDescription, +enteredPeople];
         }
     }
 
-    private clearInput(){
-        this.titleInputElement.value = "";
-        this.descriptionInputElement.value = "";
-        this.peopleInputElement.value = "";
+    private clearInput() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
     }
 
     @autobind
@@ -76,3 +129,10 @@ class ProductInput {
 }
 
 const prjInput = new ProductInput();
+
+// ?는 | undefined 와 같은 유니언 타입을 나타낸다
+
+// !== null > null만 검사
+// != null > null과 undefined를 검사
+
+// 재사용 가능한 유효성검사 만드는 방법

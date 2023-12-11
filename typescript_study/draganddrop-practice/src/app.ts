baea45b7d54308440bb3a8b4e4014b87b39d4b73
mbynae <piowm123@gmail.com>
2023-12-11 21:30:00 +0900

    TS 클래스 구독패턴 적용

--- typescript_study/draganddrop-practice/src/app.ts
+++ typescript_study/draganddrop-practice/src/app.ts
@@ -15,17 +15,37 @@ class ProjectList {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement;
+    assignedProjects: any[]; //배정할 프로젝트를 저장하기 위한 프로퍼티
 
     constructor(private type: 'active' | 'finished') {
         this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        this.assignedProjects = []; //프로퍼티 초기화
 
         const importNode = document.importNode(this.templateElement.content, true);
         this.element = importNode.firstElementChild as HTMLElement;
         this.element.id = `${this.type}-projects`;
 
+        //프로젝트를 랜더링 하기 전 입력된 엘리먼트 추가 매서드 
+        projectState.addListener((projects: any[]) =>{
+            //assignedProjects에 projects(title, description, people)를 입력
+            //renderContent가 먼저 실행되기 때문에 ${this.type}-projects-list가 존재한다.
+            this.assignedProjects = projects;
+            this.renderProject();
+        });
+
         this.renderContent();
         this.attach();
+    }
+
+    //project list 박스 안에 입력한 엘리먼트를 랜더링
+    private renderProject(){
+        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        for(const prjItem of this.assignedProjects){
+            const listItem = document.createElement("li");
+            listItem.textContent = prjItem.title;
+            listEl.appendChild(listItem);
+        }
     }
 
     private renderContent() {
@@ -40,7 +60,9 @@ class ProjectList {
 }
 
 //Project State Management (상태관리 클래스)
+//구독 패턴을 통한 입력값 저장 및 출력 방식
 class ProjectState {
+    private listener: any[] = []; // 이벤트 함수 배열
     private projects: any[] = [];
     private static instance: ProjectState; //싱글톤
 
@@ -55,6 +77,11 @@ class ProjectState {
         return this.instance;
     }
 
+    //이벤트 함수 저장
+    addListener(listenerFn: Function){
+        this.listener.push(listenerFn);
+    }
+
     addProject(title: string, description: string, numOfPeople: number) {
         const newProject = {
             id: Math.random().toString(),
@@ -63,6 +90,10 @@ class ProjectState {
             people: numOfPeople,
         };
         this.projects.push(newProject);
+        for(const listenerFn of this.listener){
+            //배열을 복사하는 방법
+            listenerFn(this.projects.slice());
+        }
     }
 }
 
@@ -163,7 +194,7 @@ class ProductInput {
         const userInput = this.gatherUserInput();
         if (Array.isArray(userInput)) {
             const [title, desc, people] = userInput;
-            console.log(title, desc, people);
+            projectState.addProject(title, desc, people); //싱글톤 인스턴스에 인자 전잘
             this.clearInput();
         }
     }

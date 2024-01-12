8fe3949e4e47a85b5e4772bf175912fd9a14e437
mbynae <piowm123@gmail.com>
2024-01-12 16:55:48 +0900

    작성중

---
+++ zustand_practice/src/store.js
@@ -0,0 +1,73 @@
+import { useLayoutEffect, useReducer, useRef } from 'react';
+import { create as createImpl } from './vanilla';
+
+export function createStoreHook(createState) {
+    const api = typeof createState === 'function' ? createImpl(createState) : createState;
+
+    const useStore = (selector = api.getState, equalityFn = Object.is) => {
+        //const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+        const [, forceUpdate] = useReducer(c => c + 1, 0);
+
+        const state = api.getState();
+
+        const stateRef = useRef(state);
+        const selectorRef = useRef(selector);
+        const equalityFnRef = useRef(equalityFn);
+        const erroredRef = useRef(false);
+
+        const currentSliceRef = useRef();
+        if (currentSliceRef.current === undefined) {
+            currentSliceRef.current = selector(state);
+        }
+
+        let newStateSlice = false;
+        let hasNewStateSlice = false;
+
+        if (stateRef.current !== state || selectorRef.current !== selector || equalityFnRef.current !== equalityFn || erroredRef.current) {
+            newStateSlice = selector(state);
+            hasNewStateSlice = !equalityFn(currentSliceRef.current, newStateSlice);
+        }
+
+        useLayoutEffect(() => {
+            if (hasNewStateSlice) {
+                currentSliceRef.current = newStateSlice;
+            }
+            stateRef.current = state;
+            selectorRef.current = selector;
+            equalityFnRef.current = equalityFn;
+            erroredRef.current = false;
+        });
+
+        const stateBeforeSubscriptionRef = useRef(state);
+        useLayoutEffect(() => {
+            const listener = () => {
+                try {
+                    const nextState = api.getState();
+                    const nextStateSlice = selectorRef.current(nextState);
+
+                    if (!equalityFnRef.current(currentSlicRef.current, nextStateSlice)) {
+                        stateRef.current = nextState;
+                        currentSliceRef.current = nextStateSlice;
+                        forceUpdate();
+                    }
+                } catch (error) {
+                    erroredRef.current = true;
+                    forceUpdate();
+                }
+            };
+            const unsubscribe = api.subscribe(listener);
+
+            if (api.getState() !== stateBeforeSubscriptionRef.current) {
+                listener();
+            }
+
+            return unsubscribe;
+        }, []);
+
+        return hasNewStateSlice ? newStateSlice : currentSlicRef.current;
+    };
+
+    Object.assign(useStore, api);
+
+    return useStore;
+}
